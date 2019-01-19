4d6c4f7a33dcbbeab862ad3f9f8f419d8c4205a0
pawelrubin <pawelrubin19@gmail.com>
2019-01-19 18:00:39 +0100

    testowansko

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,16 @@
 package controllers;
 
-import akka.actor.ActorSystem;
-import akka.stream.Materializer;
+import akka.NotUsed;
+import akka.stream.javadsl.Flow;
+import play.libs.F;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.WebSocket;
+import akka.stream.javadsl.*;
+
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -22,5 +28,30 @@ public class Application extends Controller {
     return ok(views.html.index.render());
   }
 
+//  public WebSocket socket() {
+//    return WebSocket.Text.accept(request -> {
+//      // Log events to the console
+//      Sink<String, ?> in = Sink.foreach(System.out::println);
+//
+//      // Send a single 'Hello!' message and then leave the socket open
+//      Source<String, ?> out = Source.single("Hello!").concat(Source.maybe());
+//
+//      return Flow.fromSinkAndSource(in, out);
+//    });
+//  }
+
+  public WebSocket socket() {
+    return WebSocket.Text.accept(request -> {
+      // log the message to stdout and send response back to client
+      return Flow.<String>create().map(msg -> {
+        System.out.println(msg);
+        return "I received your message: " + msg;
+      });
+    });
+  }
+
+  public Result test() {
+    return ok(views.html.socketTest.render());
+  }
 
 }
---
+++ app/models/Game.java
@@ -0,0 +1,12 @@
+package models;
+
+import akka.actor.AbstractActor;
+
+public class Game extends AbstractActor {
+
+  @Override
+  public Receive createReceive() {
+
+    return null;
+  }
+}
